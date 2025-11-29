e3e5335ac5b63b5c97cc3ba829740857f22d8b79
MRomyl <romyl.martushev@gmail.com>
2025-11-28 18:00:51 -0800

    d

--- server/app.py
+++ server/app.py
@@ -13,19 +13,16 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
-
 db.init_app(app)
 
 api = Api(app)
 
 class ClearSession(Resource):
 
-    def delete(self):
-    
-        session['page_views'] = None
-        session['user_id'] = None
+    def get(self):
+        session.clear()
+        return {"message": "Session cleared"}, 200
 
-        return {}, 204
 
 class IndexArticle(Resource):
     
@@ -33,24 +30,66 @@ class IndexArticle(Resource):
         articles = [ArticlesSchema().dump(article) for article in Article.query.all()]
         return articles, 200
 
+
 class ShowArticle(Resource):
 
     def get(self, id):
-        session['page_views'] = 0 if not session.get('page_views') else session.get('page_views')
+        if session.get('page_views') is None:
+            session['page_views'] = 0
+
         session['page_views'] += 1
 
-        if session['page_views'] <= 3:
+        if session['page_views'] > 3:
+            return {'message': 'Maximum pageview limit reached'}, 401
 
-            article = Article.query.filter(Article.id == id).first()
-            article_json = ArticlesSchema.dump(article)
+        article = Article.query.get(id)
+        if not article:
+            return {"message": "404: Article not found"}, 404
 
-            return make_response(article_json, 200)
+        return ArticlesSchema().dump(article), 200
 
-        return {'message': 'Maximum pageview limit reached'}, 401
+
+class Login(Resource):
+
+    def post(self):
+        username = request.json.get('username')
+
+        user = User.query.filter_by(username=username).first()
+
+        if not user:
+            return {"message": "User not found"}, 404
+
+        session['user_id'] = user.id
+
+        return UserSchema().dump(user), 200
+
+
+class Logout(Resource):
+
+    def delete(self):
+        session.pop('user_id', None)
+        return '', 204
+
+
+class CheckSession(Resource):
+
+    def get(self):
+        user_id = session.get('user_id')
+
+        if not user_id:
+            return {}, 401
+
+        user = User.query.get(user_id)
+
+        return UserSchema().dump(user), 200
+
 
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession, '/check_session')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
